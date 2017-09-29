1656146f8a50c4c2cc96cecd258e81c0e2ed12c5
zxcvbn96519 <zxcvbn96519@gmail.com>
2017-09-29 22:43:55 +0800

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,10 @@
+package sample;
+import javafx.event.ActionEvent;
+public class Controller {
+    public void addpress(ActionEvent event){
+        System.out.print("hello");
+    }
+    public void checkoutpress(ActionEvent event){
+        System.out.print("check");
+    }
+}
